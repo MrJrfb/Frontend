9870677c74212f8fa715aed636ac9597e57cc0b5
MrJrfb <juanrflorentin01@gmail.com>
2026-07-14 15:08:31 -0300

    Prácticas de HTML, CSS Y JS

---
+++ dia 1/practica1/script.js
@@ -0,0 +1,9 @@
+function cambiarFondo(){ // Define una función llamada cambiarFondo
+    alert("Cambiando fondo"); // Muestra una ventana emergente con el mensaje indicado
+}
+
+document.getElementById("img1").addEventListener("click",cambiarFondo);
+// Busca el elemento cuyo id es "img1" y ejecuta la función cambiarFondo cuando se hace clic sobre él
+
+document.getElementById("img2").addEventListener("click",cambiarFondo);
+// Busca el elemento cuyo id es "img2" y ejecuta la función cambiarFondo cuando se hace clic sobre él
